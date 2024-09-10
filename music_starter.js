2ab7327c16ce563c8e77055855900efa034e6261
itsrandog <emma.randlee@gmail.com>
2024-09-10 15:15:15 +1200

    soundwave function made better
    soundwave function works for each instrument

--- music_starter.js
+++ music_starter.js
@@ -7,14 +7,38 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(20);
 
   push();
-  translate(width/2, height/2);
-  soundwave(vocal, drum, bass, other);
+  translate(width/5, height/2);
+  soundwave(vocal);
+  pop();
+
+  push();
+  translate(2*width/5, height/2);
+  soundwave(drum);
+  pop();
+
+  push();
+  translate(3*width/5, height/2);
+  soundwave(bass);
+  pop();
+
+  push();
+  translate(4*width/5, height/2);
+  soundwave(other);
+  pop();
+
 }
 
-function soundwave(vocal, drum, bass, other){
+function soundwave(instrument){
   let barsize = 20;
+  let spacing = barsize+10;
 
-  let vocalheight = map(vocal, 0, 100, 1, 200);
+  let soundheight = map(instrument, 0, 100, 1, 400);
 
-  rect(0, 0, barsize, vocalheight);
+  rect(-spacing*2, 0, barsize, soundheight/4); // left bar
+  rect(-spacing, 0, barsize, soundheight/2); // left bar
+  rect(spacing, 0, barsize, soundheight/2); // right bar
+  rect(spacing*2, 0, barsize, soundheight/4); // left bar
+
+
+  rect(0, 0, barsize, soundheight); // main bar
 }

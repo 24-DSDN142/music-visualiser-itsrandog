48d19854691bdd62eeb6b0144626b03c15108b76
itsrandog <emma.randlee@gmail.com>
2024-09-10 15:00:45 +1200

    voice bar for soundwave

--- music_starter.js
+++ music_starter.js
@@ -2,5 +2,19 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  rectMode(CENTER);
+  
   background(20);
+
+  push();
+  translate(width/2, height/2);
+  soundwave(vocal, drum, bass, other);
+}
+
+function soundwave(vocal, drum, bass, other){
+  let barsize = 20;
+
+  let vocalheight = map(vocal, 0, 100, 1, 200);
+
+  rect(0, 0, barsize, vocalheight);
 }
